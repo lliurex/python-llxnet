e67b3207e70d980a8daab0de0955140a32903539
Raul Rodrigo Segura <raurodse@gmail.com>
2018-09-07 08:28:08 +0200

    add files from subversion

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(  name             = "python-llxnet",
+        version          = "0.19",
+        author           = "Hector Garcia Huerta, Raul Rodrigo Segura",
+        author_email     = "hectorgh@gmail.com, raurodse@gmail.com ",
+        url              = "http://lliurex.net/",
+        packages         = ['lliurex'],
+        package_dir      = {'lliurex': 'src/lliurex/'},
+     )
+
+
